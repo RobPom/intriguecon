2da5e1c84bc402ed760b41c51611d72284aa9820
Rob Pomerleau <pomerleau.robert@gmail.com>
2017-10-01 23:52:14 -0600

    quick temporary fix to attendee class

--- app/Attendee.php
+++ app/Attendee.php
@@ -2,17 +2,17 @@
 
 namespace App;
 
-use Laravel\Scout\Searchable;
+//use Laravel\Scout\Searchable;
 use Illuminate\Database\Eloquent\Model;
 
 class Attendee extends Model
 {
-    use Searchable;
+   /*  use Searchable;
 
     public function searchableAs()
     {
         return 'attendees_index';
-    }
+    } */
     
     public function game_timeslots(){
         return $this->belongsToMany(GameTimeslot::class);
