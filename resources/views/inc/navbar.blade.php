bf4e1a741319b96f88de8906dd0d14f2675200db
Rob Pomerleau <pomerleau.robert@gmail.com>
2017-10-18 19:48:08 -0600

    Deactivated registration and convention inforamtion

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -27,7 +27,7 @@
                 <li><a href="/articles">Articles</a></li>
                 <li><a href="/games">Games</a></li>
          
-                    <li class="dropdown">
+                    {{--  <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                             Convention<span class="caret"></span>
                         </a>
@@ -38,7 +38,7 @@
                             <li><a href="/schedule/show/{{ config('constants.active_con') }}">Schedule</a></li>
                             <li><a href="/reserve">Register</a></li>
                         </ul>
-                    </li>
+                    </li>  --}}
          
             </ul>
         </div>
