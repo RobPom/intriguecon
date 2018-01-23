ea1ca07bdd7440f9c3dad2d276c97a77c81067be
Rob Pomerleau <pomerleau.robert@gmail.com>
2018-01-22 21:48:25 -0700

    edits to paypal link

--- resources/views/pages/reserve.blade.php
+++ resources/views/pages/reserve.blade.php
@@ -27,14 +27,31 @@
               <p>If you do not receive a confirmation email within 24 hours  please contact us at  intrigue@hazardgaming.com.</p>
         </div>
     </div>
+    <hr>
     <div class="row">
         <div class="col-xs-12">
-            <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+                <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                    <input type="hidden" name="cmd" value="_s-xclick">
+                    <input type="hidden" name="hosted_button_id" value="X8M7M5HNKNFKY">
+                    <table>
+                    <tr><td><input type="hidden" name="on0" value="Number of Attendees">Number of Attendees</td></tr><tr><td><select name="os0">
+                        <option value="One person">One person $20.00 CAD</option>
+                        <option value="Two people">Two people $40.00 CAD</option>
+                        <option value="Three people">Three people $60.00 CAD</option>
+                    </select> </td></tr>
+                    </table>
+                    <br>
+                    <input type="hidden" name="currency_code" value="CAD">
+                    <input type="image" src="img/paypal.jpg" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+                <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" hidden="" style="display: none !important;">
+                    </form>
+                    <br>
+            <!-- <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
                 <input type="hidden" name="cmd" value="_s-xclick">
                 <input type="hidden" name="hosted_button_id" value="T2D6W8S2BJ4R6">
                 <input type="image" src="img/paypal.jpg" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
                 <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" hidden="" style="display: none !important;">
-            </form>
+            </form> -->
         </div>
     </div>
 </div>
