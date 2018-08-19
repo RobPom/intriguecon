81969e30fbb2841e566f747cacc6b30f10452169
Rob Pomerleau <pomerleau.robert@gmail.com>
2018-08-19 16:59:29 -0600

    missed rule added

--- app/Http/Controllers/GameProposalsController.php
+++ app/Http/Controllers/GameProposalsController.php
@@ -21,10 +21,11 @@ class GameProposalsController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
-            'title' => 'required|max:70',
+            'title' => 'required|max:60',
             'tagline' => 'required|max:140',
             'system' => 'required|max:140',
             'description' => 'required|max:1000',
+            'short_description' => 'required|max:190',
             'min' => 'required',
             'max' => 'required',
             'name' => 'required|max:70',
